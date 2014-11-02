32230b57c3ed777ae00a8007cc947fadfde6e145
Riley Shaw <rileyjshaw@gmail.com>
2014-11-02 04:53:58 -0500

    Update main.js to newest version

--- scripts/main.js
+++ scripts/main.js
@@ -1,192 +1,163 @@
-;(function ( exports ) {
-  var Game = function( width, height, id ) {
-    this.winningCombinations = [ [0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 4, 8 ], [ 2, 4, 6 ] ];
-    this.startTime = new Date;
-    this.player = 1;
-
-    this.width = width;
-    this.height = height;
-    this.id = id;
-
-    this.score1 = 0;
-    this.score2 = 0;
-    this.scoreContainer = document.getElementById( 'scores' );
-    this.scoreCounters = this.scoreContainer.childNodes;
-    this.scoreCounter1 = this.scoreCounters[0];
-    this.scoreCounter2 = this.scoreCounters[1];
-    this.turn = 0;
-    this.moves = [];
-
-    this.canvas = initCanvas( this, this.width, this.height, this.id, true );
-    document.body.insertBefore( this.canvas.element, this.scoreContainer );
-
-    this.playerRadius = 40;
-    this.canvas.context.lineWidth = 8;
-    this.canvas.context.strokeStyle = '#2c3e50';
-
-    this.canvas.paintBoard();
-  };
-
-  var initCanvas = function ( game, width, height, id, relativeMouse ) {
-    var
-    createHDCanvas,
-    clearCanvas,
-    draw,
-    paintBoard,
-    paintMessage,
-    clickHandler,
-    element,
-    context;
-
-    if ( relativeMouse || relativeMouse === undefined ) {
-      HTMLCanvasElement.prototype.relativeMouseCoords = function ( event ) {
-        var
-        totalOffsetX = 0,
-        totalOffsetY = 0,
-        canvasX = 0,
-        canvasY = 0,
-        currentElement = this;
-
-        do {
-          totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-          totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-          currentElement = currentElement.offsetParent;
-        } while ( currentElement );
-
-        canvasX = event.pageX - totalOffsetX;
-        canvasY = event.pageY - totalOffsetY;
-
-        return {
-          x: canvasX,
-          y: canvasY
-        };
-      };
-    }
-
+;(function (width, height, id) {
+  // Creates an HD canvas element on page and returns
+  // a reference to the element
+  function createCanvasElement (width, height, id, insertBefore) {
     // Creates a scaled-up canvas based on the device's
     // resolution, then displays it properly using styles
-    createHDCanvas = function ( ratio ) {
-      var
-      canvas = document.createElement( 'canvas' ),
-      pixelRatio;
+    function createHDCanvas (ratio) {
+      var canvas = document.createElement('canvas');
 
-      // Creates a dummy canvas to test device's pixel ratio
-      pixelRatio = function () {
-        var
-        context = document.createElement( 'canvas' ).getContext( '2d' ),
-        dpr = window.devicePixelRatio || 1,
-        bsr = context.webkitBackingStorePixelRatio ||
-              context.mozBackingStorePixelRatio ||
-              context.msBackingStorePixelRatio ||
-              context.oBackingStorePixelRatio ||
-              context.backingStorePixelRatio || 1;
-        return dpr / bsr;
-      };
-
-      if ( !ratio ) {
-        ratio = pixelRatio();
+      if (typeof ratio !== 'number') {
+        // Creates a dummy canvas to test device's pixel ratio
+        ratio = (function () {
+          var ctx = document.createElement('canvas').getContext('2d');
+          var dpr = window.devicePixelRatio || 1;
+          var bsr = ctx.webkitBackingStorePixelRatio ||
+                ctx.mozBackingStorePixelRatio ||
+                ctx.msBackingStorePixelRatio ||
+                ctx.oBackingStorePixelRatio ||
+                ctx.backingStorePixelRatio || 1;
+          return dpr / bsr;
+        })();
       }
 
       canvas.width = width * ratio;
       canvas.height = height * ratio;
       canvas.style.width = width + 'px';
       canvas.style.height = height + 'px';
-      canvas.getContext( '2d' ).setTransform( ratio, 0, 0, ratio, 0, 0 );
+      canvas.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
       canvas.id = id;
 
       return canvas;
-    };
+    }
 
-    clearCanvas = function () {
-      context.clearRect(0, 0, width, height);
-    };
+    var canvas = createHDCanvas();
+    document.body.insertBefore(canvas, insertBefore);
+    return canvas;
+  }
 
-    draw = function ( player, x, y ) {
-      if ( player ) { // X
-        context.moveTo(x - game.playerRadius, y - game.playerRadius);
-        context.lineTo(x + game.playerRadius, y + game.playerRadius);
-        context.moveTo(x - game.playerRadius, y + game.playerRadius);
-        context.lineTo(x + game.playerRadius, y - game.playerRadius);
-      } else { // O
-        context.beginPath();
-        context.arc( x, y, game.playerRadius, 0, 2 * Math.PI );
-      }
-      context.stroke();
-    };
+  // Returns mouse coordinates that are
+  // relative to the canvas, i.e. useful
+  function relativeCoords (event) {
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var currentElement = game;
 
-    paintBoard = function () {
-      clearCanvas();
-      context.beginPath();
+    do {
+      totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+      totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+      currentElement = currentElement.offsetParent;
+    } while (currentElement);
 
-      for ( var i = 3; i >= 0; i-- ) {
-        context.moveTo( ( width / 3 ) * i, 0 );
-        context.lineTo( ( width / 3 ) * i, height );
-
-        context.moveTo( 0, ( height / 3 ) * i );
-        context.lineTo( width, ( height / 3 ) * i );
-      }
-
-      context.stroke();
-      context.closePath();
-    };
-
-    paintMessage = function ( message ) {
-      clearCanvas();
-      context.fillStyle = '#2c3e50';
-      context.fillRect( 0, 0, width, height );
-      context.fillStyle = '#9b59b6';
-      context.font = "18px sans-serif";
-      context.fillText( message, 5, 20 );
-    };
-
-    element = createHDCanvas();
-    context = element.getContext( '2d' );
+    var canvasX = event.pageX - totalOffsetX;
+    var canvasY = event.pageY - totalOffsetY;
 
     return {
-      element: element,
-      context: context,
-      clearCanvas: clearCanvas,
-      clickHandler: clickHandler,
-      draw: draw,
-      paintBoard: paintBoard,
-      paintMessage: paintMessage
+      x: canvasX,
+      y: canvasY
     };
+  }
+
+  function drawMessage (message) {
+      if (typeof message === 'string') {
+        if (messageContainer.className) messageContainer.className = '';
+        messageContainer.textContent = message;
+      } else messageContainer.className = 'noMessage';
+  }
+
+  function drawPlayer (i, player) {
+    var x = width * (2 * (i % 3) + 1) / 6;
+    var y = height * (2 * (Math.floor(i / 3)) + 1) / 6;
+
+    ctx.beginPath();
+
+    if (player === 'X') {
+      ctx.moveTo(x - playerRadius, y - playerRadius);
+      ctx.lineTo(x + playerRadius, y + playerRadius);
+      ctx.moveTo(x - playerRadius, y + playerRadius);
+      ctx.lineTo(x + playerRadius, y - playerRadius);
+    } else if (player === 'O') {
+      ctx.beginPath();
+      ctx.arc(x, y, playerRadius, 0, 2 * Math.PI);
+    } else throw 'Invalid player argument ' + player;
+
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  function handleClick (event) {
+    var coords = relativeCoords(event);
+    var x = Math.floor(coords.x * 3 / width);
+    var y = Math.floor(coords.y * 3 / height);
+    var i = x + y * 3;
+    var clickTime = new Date().getTime();
+    socket.emit('move', i);
+  }
+
+  function countdown (n) {
+    n = n || 3;
+    (function step () {
+      if (n) {
+        drawMessage('Starting round in ' + n--);
+        setTimeout(step, 400);
+      } else drawMessage();
+    })();
+  }
+
+  var socket = io('http://localhost:8090');
+  var player;
+  var scores = {
+    X: 0,
+    O: 0
   };
 
-  var clickHandler = function ( event, game ) {
-    var
-    coords = game.canvas.element.relativeMouseCoords( event ),
-    x = Math.floor( coords.x * 3 / game.width ),
-    y = Math.floor( coords.y * 3 / game.height ),
-    index = x + y * 3,
-    clickTime = new Date;
+  socket.on('startGame', function () {
+    countdown();
+  });
 
-    if ( typeof( game.moves[ index ] ) === 'undefined' ) {
-      var
-      xPixels = game.width * ( 2 * x + 1 ) / 6;
-      yPixels = game.height * ( 2 * y + 1 ) / 6;
+  socket.on('setPlayer', function (character) {
+    player = character;
+  });
 
-      game.moves[ index ] = game.player;
-      game.canvas.draw( game.player, xPixels, yPixels );
-      game.canvas.context.stroke();
-
-      console.log('Received move "' + game.player + '" at time ' + ( clickTime - game.startTime ) );
-
-      if ( ++game.turn === 9 ) {
-        game.turn = 0;
-        game.moves = [];
-
-        game.canvas.paintBoard();
+  socket.on('moveAck', function (result, square, player) {
+    if (result !== null) {
+      drawPlayer(square, player);
+      if (result) {
+        drawMessage('Team ' + player + ' won!');
+        ctx.clearRect(0, 0, width, height);
+        (player === 'X' ? score1 : score2).innerText = ++scores[player];
+        setTimeout(countdown, 1000);
       }
-
     }
-  };
+  });
 
-  document.addEventListener( 'DOMContentLoaded', function () {
-    var game = new Game( 400, 400, 'tictactoe' );
-    game.canvas.element.addEventListener( 'click', function ( event ) {
-      clickHandler( event, game );
-    }, false);
-  }, false );
+  var scoreContainer = document.getElementById('scores');
+  var scoreX = document.getElementById('score1');
+  var scoreO = document.getElementById('score2');
+  var messageContainer = document.getElementById('messageContainer');
+  var game = createCanvasElement(width, height, 'game', scoreContainer);
+  var bg = createCanvasElement(width, height, 'bg', game);
+  var ctx = game.getContext('2d');
+  var bgCtx = bg.getContext('2d');
 
-})( this );
+  var playerRadius = 40;
+  ctx.lineWidth = bgCtx.lineWidth = 8;
+  ctx.strokeStyle = bgCtx.strokeStyle = '#336ACD';
+
+  bgCtx.beginPath();
+
+  for (var i = 3; i >= 0; i--) {
+    bgCtx.moveTo((width / 3) * i, 0);
+    bgCtx.lineTo((width / 3 ) * i, height);
+
+    bgCtx.moveTo(0, (height / 3) * i);
+    bgCtx.lineTo(width, (height / 3) * i);
+  }
+
+  bgCtx.stroke();
+  bgCtx.closePath();
+
+  game.addEventListener('click', function (event) {
+    handleClick(event);
+  }, false);
+})(400, 400);
